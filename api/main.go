282b7211fc77a989ae35308cafbf88bd2de50f61
Madhan Ganesh L <madhan.l@yodlee.com>
2022-01-04 09:21:42 +0530

    ui build copies dist to API public location

--- api/main.go
+++ api/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	_ "github.com/mattn/go-sqlite3"
@@ -35,9 +36,11 @@ func main() {
 	}
 
 	router := chi.NewRouter()
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello from DoApp!")
-	})
+
+	workDir, _ := os.Getwd()
+	filesDir := http.Dir(filepath.Join(workDir, "public"))
+	FileServer(router, "/", filesDir)
+
 	router.Post("/user", addUser)
 	router.Get("/user", getUsers)
 
@@ -49,6 +52,27 @@ func main() {
 	log.Printf("Starting server in 8080...")
 
 	log.Fatal(server.ListenAndServe())
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
 
 type User struct {
